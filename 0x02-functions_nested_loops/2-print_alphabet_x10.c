53aa8da5fcb7c4ed961f897f4e5a9e94f41254a4
Chinwike Maduabuchi <chinwikedev@gmail.com>
2023-03-22 06:44:29 -0700

    solve ques 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,10 +12,11 @@ void print_alphabet_x10(void)
 	int x;
 
 	for (x = 1; x <= 10; x++)
+	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
-			_putchar('\n');
 		}
-	printf('\n');
+		_putchar('\n');
+	}
 }
